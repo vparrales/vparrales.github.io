b5023c32854b4709b846bec5c89e3c8ad514c14d
vparrales <37681990+vparrales@users.noreply.github.com>
2018-10-18 22:35:18 -0700

    Add files via upload

---
+++ cnit132/firstphp.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Using a link with event handler</title>
+<style type="text/css">
+	h3 {color:#FF0000; font-type:italic; text-align:center;}
+</style>
+</head>
+<body>
+
+ <h3>A simple PHP code showing a message</h3>
+
+ <p>
+
+ <?php
+ //Command print to print a message
+ /*The following command will print
+ Our frist PHP code on the screen */
+
+ print ("Our first PHP code!");
+
+ ?>
+ </p>
+</body>
+</html>
